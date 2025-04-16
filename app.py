577236dd18be1572c4655ea5775b4d4c79c2fe02
ransomw4re <wildzer0@protonmail.com>
2025-04-16 09:08:10 +0200

    reworked project tree

--- app.py
+++
@@ -1,8 +0,0 @@
-from notebook import Notebook
-
-if __name__ == "__main__":
-    filename = "files/reverse_eng"
-
-    notebook = Notebook(f"{filename}.note")
-    notebook.print_metadata()
-    notebook.export_pdf()
--- lib/parser.py
+++ lib/parser.py
@@ -1,7 +1,6 @@
 import re
 import os
 import io
-import ast
 import json
 
 
